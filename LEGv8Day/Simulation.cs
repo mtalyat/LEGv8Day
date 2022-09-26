631abd98e3761a882b7cb6fb2448a755e6efa2c1
Mitchell Talyat <mitchell.talyat@gmail.com>
2022-09-26 12:53:38 -0500

    Simulation documentation

--- LEGv8Day/Simulation.cs
+++ LEGv8Day/Simulation.cs
@@ -6,24 +6,73 @@ using System.Threading.Tasks;
 
 namespace LEGv8Day
 {
+    /// <summary>
+    /// Responsibly for running the simulation of the LEGV8 code.
+    /// </summary>
     public class Simulation
     {
+        #region Configuration
+
+        /// <summary>
+        /// The number of registers within this simulation.
+        /// </summary>
         public const int REGISTER_COUNT = 32;
+
+        /// <summary>
+        /// The size of memory, in bytes, within this simulation.
+        /// </summary>
         public const int MEMORY_SIZE = 4096;
 
+        #endregion
+
+        #region Registers
+
+        /// <summary>
+        /// The index of the stack pointer register.
+        /// </summary>
         public const int STACK_POINTER_REG = REGISTER_COUNT - 4;
+
+        /// <summary>
+        /// The index of the frame pointer register.
+        /// </summary>
         public const int FRAME_POINTER_REG = REGISTER_COUNT - 3;
+
+        /// <summary>
+        /// The index of the return address register.
+        /// </summary>
         public const int RETURN_ADDRESS_REG = REGISTER_COUNT - 2;
+
+        /// <summary>
+        /// The index of the constant zero register.
+        /// </summary>
         public const int ZERO_REG = REGISTER_COUNT - 1;
 
+        #endregion
+
+        /// <summary>
+        /// The registers within this simulation.
+        /// </summary>
         private readonly long[] _registers;
 
+        /// <summary>
+        /// The memory within this simulation.
+        /// </summary>
         private readonly byte[] _memory;
 
+        /// <summary>
+        /// The instructions that are used to run this simulation.
+        /// </summary>
         private readonly Instruction[] _instructions;
 
+        /// <summary>
+        /// The current index of the instruction that is being executed.
+        /// </summary>
         public int ExecutionIndex { get; set; } = 0;
 
+        /// <summary>
+        /// Creates a new Simulation that will run using the given instructions.
+        /// </summary>
+        /// <param name="instructions"></param>
         public Simulation(Instruction[] instructions)
         {
             _registers = new long[REGISTER_COUNT];
@@ -31,6 +80,9 @@ namespace LEGv8Day
             _instructions = instructions;
         }
 
+        /// <summary>
+        /// Runs the simulation.
+        /// </summary>
         public void Run()
         {
             Reset();
@@ -40,7 +92,7 @@ namespace LEGv8Day
 
             Instruction currentInstruction;
 
-            while(ExecutionIndex < _instructions.Length)
+            while (ExecutionIndex < _instructions.Length)
             {
                 currentInstruction = _instructions[ExecutionIndex];
 
@@ -50,6 +102,9 @@ namespace LEGv8Day
             }
         }
 
+        /// <summary>
+        /// Resets the simulation entirely. Clears all registers and memory.
+        /// </summary>
         public void Reset()
         {
             //clear registers
@@ -65,6 +120,10 @@ namespace LEGv8Day
             }
         }
 
+        /// <summary>
+        /// Provides a string array including all data from registers, memory, and instructions.
+        /// </summary>
+        /// <returns></returns>
         public string[] Dump()
         {
             List<string> results = new List<string>();
@@ -108,6 +167,14 @@ namespace LEGv8Day
             return results.ToArray();
         }
 
+        /// <summary>
+        /// Reinterprets the given T t as a U.
+        /// Equivalent to the C++ reinterpret_cast.
+        /// </summary>
+        /// <typeparam name="T">The source type.</typeparam>
+        /// <typeparam name="U">The destination type.</typeparam>
+        /// <param name="t">The original value.</param>
+        /// <returns>A U, which has the same exact bits as the given T t, but is a type of U.</returns>
         private static U Reinterpret<T, U>(T t) where T : unmanaged where U : unmanaged
         {
             U u;
@@ -120,23 +187,51 @@ namespace LEGv8Day
             return u;
         }
 
+        /// <summary>
+        /// Gets the value from the register with the given index.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
         public long GetReg(int index)
         {
             return _registers[index];
         }
 
+        /// <summary>
+        /// Gets the value from the register with the given index, and reinterprets it to the given type T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="index"></param>
+        /// <returns></returns>
         public T GetReg<T>(int index) where T : unmanaged => Reinterpret<long, T>(GetReg(index));
 
-        public void SetReg(int index, long data)
+        /// <summary>
+        /// Sets the value for the register with the given index to the given value.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        public void SetReg(int index, long value)
         {
-            _registers[index] = data;
+            _registers[index] = value;
         }
 
-        public void SetReg<T>(int index, T data) where T : unmanaged => SetReg(index, Reinterpret<T, long>(data));
-        
-        public void SetMem(int index, long data, int size)
+        /// <summary>
+        /// Reinterprets the given value to a long, and then sets the value for the register with the given index to the given value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        public void SetReg<T>(int index, T value) where T : unmanaged => SetReg(index, Reinterpret<T, long>(value));
+
+        /// <summary>
+        /// Sets the memory at the given index to the given value.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        /// <param name="size"></param>
+        public void SetMem(int index, long value, int size)
         {
-            PackedLong p = new PackedLong(data);
+            PackedLong p = new PackedLong(value);
 
             for (int i = 0; i < size && i + index < MEMORY_SIZE; i++)
             {
@@ -144,8 +239,30 @@ namespace LEGv8Day
             }
         }
 
-        public void SetMem<T>(int index, T data, int size) where T : unmanaged => SetMem(index, Reinterpret<T, long>(data), size);
+        /// <summary>
+        /// Reinterprets the given value to a long, then sets the value within the memory. Uses the size in bytes of T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="index"></param>
+        /// <param name="value"></param>
+        public void SetMem<T>(int index, T value) where T : unmanaged
+        {
+            int size;
 
+            unsafe
+            {
+                size = sizeof(T);
+            }
+
+            SetMem(index, Reinterpret<T, long>(value), size);
+        }
+
+        /// <summary>
+        /// Gets the memory from the given position and given size as a long.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
         public long GetMem(int index, int size)
         {
             PackedLong p = new PackedLong();
@@ -158,18 +275,46 @@ namespace LEGv8Day
             return p.Long;
         }
 
-        public T GetMem<T>(int index, int size) where T : unmanaged => Reinterpret<long, T>(GetMem(index, size));
+        /// <summary>
+        /// Gets the memory from the given position and reinterprets it as a T. Uses the size in bytes of T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T GetMem<T>(int index) where T : unmanaged
+        {
+            int size;
 
+            unsafe
+            {
+                size = sizeof(T);
+            }
+
+            return Reinterpret<long, T>(GetMem(index, size));
+        }
+
+        /// <summary>
+        /// Gets all register values.
+        /// </summary>
+        /// <returns></returns>
         public long[] GetRegisters()
         {
             return _registers;
         }
 
+        /// <summary>
+        /// Gets all memory values.
+        /// </summary>
+        /// <returns></returns>
         public byte[] GetMemory()
         {
             return _memory;
         }
-
+        
+        /// <summary>
+        /// Gets all instructions.
+        /// </summary>
+        /// <returns></returns>
         public Instruction[] GetInstructions()
         {
             return _instructions;
