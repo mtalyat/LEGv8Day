8135adbee9c30f5cd67e42be7e25a021a7efdd59
Mitchell Talyat <mitchell.talyat@gmail.com>
2022-09-28 14:30:40 -0500

    Data documentation

--- LEGv8Day/Data.cs
+++ LEGv8Day/Data.cs
@@ -13,20 +13,35 @@ namespace LEGv8Day
     /// </summary>
     internal static class Data
     {
-        private static string SaveFolder => Application.UserAppDataPath;
+        /// <summary>
+        /// The directory in which data is saved.
+        /// </summary>
+        private static string SaveDirectory => Application.UserAppDataPath;
 
+        /// <summary>
+        /// Saves the given object as a json text file at the given file path.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <param name="obj"></param>
         public static void Save<T>(string path, T? obj)
         {
-            string fullPath = Path.Combine(SaveFolder, path);
+            string fullPath = Path.Combine(SaveDirectory, path);
 
             string json = JsonConvert.SerializeObject(obj);
 
             File.WriteAllText(fullPath, json);
         }
 
+        /// <summary>
+        /// Loads the json data from the given file path.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns>Null if there is no file, or no text in the file.</returns>
         public static T? Load<T>(string path)
         {
-            string fullPath = Path.Combine(SaveFolder, path);
+            string fullPath = Path.Combine(SaveDirectory, path);
 
             if (!File.Exists(fullPath))
             {
